6742db11c57fedcaf319b2b541a2eb820130a4e7
Parkteria <77946625+Parkteria@users.noreply.github.com>
2025-08-10 22:07:19 +0900

    coco_eval.py

--- coco_eval.py
+++ coco_eval.py
@@ -21,7 +21,7 @@ class CocoEvaluator:
         self.coco_eval = {}
         for iou_type in iou_types:
             coco_eval = COCOeval(coco_gt, iouType=iou_type)
-            coco_eval.params.kpt_oks_sigmas = np.array([0.1, 0.1, 0.1, 0.3, 0.1, 0.3, 0.1, 0.1]) / 10.0
+            coco_eval.params.kpt_oks_sigmas = np.array([0.2, 0.2, 0.2, 0.6, 0.2, 0.6, 0.2, 0.2]) / 10.0
             self.coco_eval[iou_type] = coco_eval
 
         self.img_ids = []
